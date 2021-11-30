ab07a55dc290c6da91ce3292ec8fe78c478af92e
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2021-11-30 12:42:12 +0000

    fixed the bug on the design square of the articles page

--- js/articles.js
+++ js/articles.js
@@ -8,10 +8,10 @@ window.addEventListener("load", () => {
     let designSquare = document.querySelector(".design-square1");
      if(translateElement.innerHTML === ""){
          designSquare.style.left ="50vw";
-         designSquare.style.height = "180vw"
+         designSquare.style.height = "173vw"
          designSquare.style.transform = "translate(-50%, 0)";
      } else {
-         designSquare.style.height = "180vw"
+         designSquare.style.height = "173vw"
         designSquare.style.transform = "translate(-60%, 0)";
      }
 })
